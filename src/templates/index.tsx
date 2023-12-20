967e520d233c9444f880a049fd670d1930f6df73
Duval Kilpatrick <mkilpatrick@yext.com>
2023-12-20 13:31:04 -0700

    init pagesjs

---
+++ src/templates/index.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {
+  Template,
+  GetPath,
+  GetHeadConfig,
+  HeadConfig,
+  TransformProps,
+  TemplateConfig,
+  TemplateProps,
+  TemplateRenderProps,
+} from "@yext/pages";
+
+/**
+ * Defines the path that the generated file will live at for production.
+ *
+ * NOTE: This currently has no impact on the local dev path. Local dev urls currently
+ * take on the form: featureName/entityId
+ */
+export const getPath: GetPath<TemplateRenderProps> = () => {
+  return `index.html`;
+};
+
+/**
+ * This allows the user to define a function which will take in their template
+ * data and produce a HeadConfig object. When the site is generated, the HeadConfig
+ * will be used to generate the inner contents of the HTML document's <head> tag.
+ * This can include the title, meta tags, script tags, etc.
+ */
+export const getHeadConfig: GetHeadConfig<
+  TemplateRenderProps
+> = (): HeadConfig => {
+  return {
+    title: "Yext UI Kit Evaluation",
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+  };
+};
+
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct result from `transformProps`.
+ */
+const Static: Template<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  document,
+  __meta,
+}) => {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
+};
+
+export default Static;
